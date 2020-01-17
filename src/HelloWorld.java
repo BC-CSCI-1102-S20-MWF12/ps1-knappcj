c7caa88a1a39eca69abd08256caf9cb6165db121
Christopher Knapp <knappce@bc.edu>
2020-01-17 17:10:50 -0500

    Added comment to HelloWorld file

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,7 @@
 public class HelloWorld {
 
   public static void main (String[] args) {
-    System.out.println("Bonjour!");
+    System.out.println("Bonjour!"); //changed "hello world" to Bonjour
   }
 
 }
